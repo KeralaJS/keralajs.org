4c3b5b38628ad32793376032693bd9a1fed44d13
Shameer C <shameer806@gmail.com>
2012-02-12 23:07:03 +0530

    edit

--- migrate.php
+++ migrate.php
@@ -3,7 +3,7 @@ $host = "tunnel.pagodabox.com";
 $user = "yulanda";
 $pass = "Glzo1YZS";
 $db = 'mysql';
-$mysql = new Mysqli($host,$user,$pass,$db,3306);
+$mysql = new mysqli($host,$user,$pass,$db,3306);
 $res = $mysql->query("create database test");
 var_dump($res);
 ?>
