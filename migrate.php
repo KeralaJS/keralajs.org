03f8132e1b46b0694579c9aacf9d03ad94790556
shameerc <shameer806@gmail.com>
2015-01-31 20:53:57 +0400

    remove unwanted files

--- migrate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "tunnel.pagodabox.com";
-$user = "yulanda";
-$pass = "Glzo1YZS";
-$db = 'mysql';
-//$mysql = new mysqli($host,$user,$pass,$db,3306);
-//$res = $mysql->query("create database test");
-//var_dump($res);
-?>
